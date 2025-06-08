8e103445656c4cd77efe00883fa36fb60c6db53f
example <dev@example.com>
2025-06-08 22:30:05 +0800

    更新代码

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const apiUrl = 'https://airplay-api.artools.cc'
+const socketUrl = 'wss://airplay-api.artools.cc/api/wss'
+const defaultCover = 'https://iph.href.lu/360x528?text=PICTURE&fg=bcbcbc&bg=eeeeee'
+
+export {apiUrl, socketUrl}
