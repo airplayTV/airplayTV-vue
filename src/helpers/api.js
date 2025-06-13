8119ef3076454fc1f1352135f590243755bd8060
game.desktop <game.desktop@example.com>
2025-06-13 15:40:46 +0800

    修改样式

--- src/helpers/api.js
+++ src/helpers/api.js
@@ -1,18 +1,18 @@
-import { httpInstance } from './request'
+import {httpInstance} from './request'
 
 const httpSourceList = () => {
   return httpInstance.get('/api/video/provider')
 }
 
-const httpVideoList = (tag , page, _source) => {
+const httpVideoList = (tag, page, _source) => {
   return httpInstance.get(`/api/video/list?tag=${tag}&page=${page}&_source=${_source}`)
 }
 
-const httpVideo = (id , _source) => {
+const httpVideo = (id, _source) => {
   return httpInstance.get(`/api/video/detail?id=${id}&_source=${_source}`)
 }
 
-const httpVideoSource = (vid , pid , _source, _m3u8p) => {
+const httpVideoSource = (vid, pid, _source, _m3u8p) => {
   return httpInstance.get(`/api/video/source?vid=${vid}&pid=${pid}&_source=${_source}&_m3u8p=${_m3u8p}`)
 }
 
@@ -20,8 +20,8 @@ const httpVideoSearch = (query) => {
   return httpInstance.get(`/api/video/search?${query}`)
 }
 
-const apiVideoSearchSSE = (keyword, page , source, sourceMode) => {
-  return `/api/sse/video/search?page=${page}&keyword=${keyword}&_source=${source}&source_mode=${sourceMode}`
+const apiVideoSearchSSE = (keyword, page, source, sourceMode) => {
+  return `/api/sse/video/search?page=${page}&keyword=${keyword}&_source=${source}&_mode=${sourceMode}`
 }
 
 const httpPlayUrlNetworkCheck = (url) => {
