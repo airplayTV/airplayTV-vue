cff0d0b92f21ce69cdff444c6f14bb48620c36a9
game.desktop <game.desktop@example.com>
2025-12-12 10:51:59 +0800

    修改样式

--- src/config.js
+++ src/config.js
@@ -3,6 +3,7 @@ const socketUrl = 'wss://airplay-api.artools.cc/api/wss'
 
 // // 本地调试
 // const apiUrl = 'http://127.0.0.1:8082'
+// const socketUrl = 'wss://airplay-api.artools.cc/api/wss'
 
 // // 本地打包，域名同api服务中配置
 // const apiUrl = 'https://airplay-tv.pages.dev'
